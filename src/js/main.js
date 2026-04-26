7aba3ae58e465cecdf5b2a0056f2ca8cd9d62e90
ej6612 <ej_6612@outlook.com>
2026-04-25 23:27:41 -0600

    W01 Prove

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+  return `<img src="${info.images[0].url}" alt="Yellowstone Park" />
+          <div class="hero-banner__content">
+            <a href="#" class="hero-banner__title">${info.name}</a>
+            <p class="hero-banner__subtitle">
+                <span>${info.designation}</span>
+                <span>${info.states}</span>
+            </p>
+          </div>`;
+}
+
+const hero = document.querySelector(".hero-banner");
+hero.innerHTML = parkInfoTemplate(parkData);
+
